4997cb729eca1da5ba3aac89455eca5a87da42ef
4617azhar <4617azhar@gmail.com>
2022-10-04 15:23:40 +0530

    first js

---
+++ main.js
@@ -0,0 +1,69 @@
+// document.getElementById("one").innerHTML = "Hello js"
+
+
+// let name="javascript"
+// name="Manager"
+// let a,b,c
+// a=10
+// b=40
+// c=a+b
+// console.log(c)
+// console.log(name)
+
+
+// var flower ="Banana"
+// var flower ="rose"
+// console.log(flower)
+
+// place ="Khalilabad";
+// console.log(typeof place)
+// var place;
+
+
+// const bird = "hen"
+// console.log(bird)
+
+// const social ="media"
+// console.log(typeof social)
+
+// Arithematic Operator
+// let a,b,c,c1,c2,c3,c4;
+// a=10
+// b=8
+// c=a+b
+// c1=a-b
+// c2=a*b
+// c3=a/b
+// c4=a%b
+// console.log(c,c1,c2,c3,c4)
+
+// Assignment Operators
+// let a,b
+// a=10
+// a+=1
+// b=12
+// b-=1
+// console.log(a,b)
+
+
+// Comparison Operators
+// let a,b,c,d;
+// a=5
+// b=6
+// c=10
+// d=23
+// console.log(a<b, a==c, c!=d)
+
+// Logical Operator
+let x,y,z
+x=12
+y=34
+z=3
+x && y
+
+
+
+
+
+
+
